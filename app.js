bc5364869635347850284df17e5bd4bb83eb9ca3
Yaseen AP <yaseenap148@gmail.com>
2023-04-30 21:31:01 +0530

    second commit

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose')
-mongoose.connect("mongodb://127.0.0.1:27017/Hoodhub") 
+require('dotenv').config()
+
+mongoose.connect(process.env.MONGO) 
 
 // express------------>
 const express = require('express')
